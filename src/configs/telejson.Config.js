78e41f2401db792cebd427d57f669a881e5f1a19
Robert Dumitrescu <robertdum01@gmail.com>
2020-10-29 02:29:14 +0000

    added the new interpolation paradigm

---
+++ src/configs/telejson.Config.js
@@ -0,0 +1,5 @@
+let teleJsonConfig = {
+    space: 4
+}
+
+export {teleJsonConfig};
---
+++ src/services/operations.Service.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ *
+ * @classdesc Class that deals with interpolation operations that are part of interpolation processing, replace and analyses.
+ * @author Robert Dumitrescu (LinkedIn: https://www.linkedin.com/in/robertdumitrescu/) (Github: https://github.com/robertdumitrescu)
+ * @class OperationsService
+ */
+class OperationsService {
+    /**
+     * @TODO 
+     * @typedef {Object} InterpolateOptions
+     * */
+
+    /**
+     * @TODO
+     * @typedef {Object} InterpolateResult
+     * */
+
+    /**
+     * @description Given an string with interpolations, it will try to interpolated based on an given analysis or it will try to analyze first and then interpolate
+     * @author Robert Dumitrescu (LinkedIn: https://www.linkedin.com/in/robertdumitrescu/) (Github: https://github.com/robertdumitrescu)
+     * @static
+     * @param {InterpolateOptions} options - The options according to which the method will interpolate values within a string
+     * @returns {InterpolateOptions}
+     * @memberof OperationsService
+     */
+    static interpolate(options) {
+
+    }
+}
+
+export {OperationsService};
